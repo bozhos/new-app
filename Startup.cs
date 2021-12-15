f987606f23e4b0d7f4c54f0d6f6a157da8892485
Bozhidar Stanishovski <bozidarbt@gmail.com>
2021-12-15 09:47:08 +0100

    changed hello bozho

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace new_app
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello Bozho!");
                 });
             });
         }
